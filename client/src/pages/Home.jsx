ca98b7eb3349c0e726d43c6332d557328312ab2c
Md Moniruzzaman <mdmoniruzzamanbillal@gmail.com>
2024-02-17 09:04:31 +0600

    set up

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -16,6 +16,9 @@ export default function Home() {
       <h1>home page </h1>
       <h1>home page </h1>
       <h1>home page </h1>
+      <h1>Monir </h1>
+      <h1>Monir </h1>
+      <h1>Monir </h1>
     </div>
   );
 }
