09f543fc2131cf54440a36959e5b25f5bae24c5c
Md Moniruzzaman <mdmoniruzzamanbillal@gmail.com>
2024-02-17 13:59:24 +0600

    learmning ground page : explaination section added ( static)

--- client/src/pages/Test.jsx
+++ client/src/pages/Test.jsx
@@ -1,32 +1,37 @@
 import React from "react";
 
+// const dummyData = `public class HelloWorld {\npublic static void main(String[] args) {\nSystem.out.println("hello worlds");\n}\n} `;
+const dummyData = [
+  "public class Main {",
+  "    public static void main(String[] args) {",
+  '        System.out.println("hello worlds");',
+  "    }",
+  "}",
+];
+
 const Test = () => {
+  //   console.log(dummyData.split("\n"));
+  //   const dummyResponse = dummyData.split("\n");
+
+  //   console.log(dummyResponse);
+
+  const handleCodeLine = (data) => {
+    console.log(data);
+  };
+
   return (
     <div className="learningGroudContainer">
       <div className="learningGroudWrapper">
         {/* input container starts  */}
-        <div className="inputContainer">
+        <div className="inputContainer  ">
           {/* input container top input  */}
-          <div className="userSelectInputSection">
-            <label
-              htmlFor="category"
-              className="block mb-2  font-medium text-gray-900 "
-            >
-              User language
-            </label>
-            <select
-              id="user_language"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 "
-            >
-              <option value="">Select language</option>
-              <option value="">JavaScript</option>
-              <option value="">Java</option>
-            </select>
+          <div className="userSelectInputSection   ">
+            <p className="  text-xl font-medium ">Js (input)</p>
           </div>
           {/* input container top input  */}
 
           {/* user code input container starts   */}
-          <div className="userCode mt-4 ">
+          <div className="userCode mt-2  ">
             <textarea
               id="message"
               rows="4"
@@ -39,33 +44,51 @@ const Test = () => {
         {/* input container ends  */}
 
         {/* output container starts  */}
-        <div className="outputContainer  mt-[3rem] ">
+        <div className="outputContainer  mt-[2.5rem] ">
           {/* user output language select input starts  */}
           <div className="outputLanguageinput">
-            <label
-              htmlFor="category"
-              className="block mb-2  font-medium text-gray-900 "
-            >
-              Output
-            </label>
-            <select
-              id="user_language"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 "
-            >
-              <option value="">Select language</option>
-              <option value="">JavaScript</option>
-              <option value="">Java</option>
-            </select>
+            <p className="  text-xl font-medium ">Java (Output)</p>
           </div>
           {/* user output language select input ends  */}
 
+          {/* output response container starts  */}
+          <div className="outputContainer mt-[1rem] flex flex-col gap-y-0   p-3 bg-gray-200  ">
+            {dummyData &&
+              dummyData?.map((code, ind) => (
+                <p
+                  className="codeLine   p-1  cursor-pointer rounded  "
+                  onClick={() => handleCodeLine(code)}
+                >
+                  <pre>{code}</pre>
+                </p>
+              ))}
+          </div>
+          {/* output response container starts  */}
+
           {/* convert button starts  */}
-          <div className="convertBtn mt-[1rem] ">
+          <div className="convertBtn mt-[1.5rem] ">
             <button className=" py-2 px-4 bg-green-600 text-gray-50 font-medium rounded hover:bg-green-700 hover:shadow-lg hover:scale-105 active:scale-100   ">
-              Convert
+              Generate
             </button>
           </div>
           {/* convert button ends */}
+
+          {/* explain container starts  */}
+          <div className="explainSection  mt-[2.5rem]  ">
+            {/* explaination title starts  */}
+
+            <h1 className="  mb-3  font-semibold text-xl ">
+              Explaination (out) :
+            </h1>
+
+            {/* explaination title ends */}
+
+            <p className=" bg-gray-200 text-lg   p-3 rounded   ">
+              my explaination my explaination my explaination my explaination my
+              explaination
+            </p>
+          </div>
+          {/* explain container ends  */}
         </div>
         {/* output container ends */}
       </div>
